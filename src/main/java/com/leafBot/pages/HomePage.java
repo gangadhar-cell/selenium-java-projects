f91aec2d6ec49f831157c046d7cc400e6c3567ce
gangadhar-cell <mgangadharece@gmail.com>
2019-11-12 09:22:26 +0530

    updated in home page syso

--- src/main/java/com/leafBot/pages/HomePage.java
+++ src/main/java/com/leafBot/pages/HomePage.java
@@ -5,6 +5,7 @@ import com.leafBot.selenium.api.base.SeleniumBase;
 public class HomePage extends SeleniumBase{
 	public HomePage clickLogout() {
 		click(locateElement("class","decorativeSubmit"));
+		System.out.println("hii");
 		System.out.println();
 		return new HomePage();
 	}
